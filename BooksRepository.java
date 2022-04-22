9cf6f76be20a58177ac809d5f7861d6693ae1df1
ashwinithombare212 <104186861+ashwinithombare212@users.noreply.github.com>
2022-04-22 13:00:16 +0530

    Add files via upload

---
+++ BooksRepository.java
@@ -0,0 +1,7 @@
+package com.ashwini.repository;
+import org.springframework.data.repository.CrudRepository;
+//repository that extends CrudRepository
+import com.javatpoint.model.Books;
+public interface BooksRepository extends CrudRepository<Books, Integer>
+{
+}
